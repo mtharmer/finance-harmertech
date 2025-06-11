8cd862f4c6b16812624811786aefeac48367a79b
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-11 11:47:37 -0400

    HT-44 | Change background and remove console statements

--- src/utility/doesSessionExist.js
+++ src/utility/doesSessionExist.js
@@ -2,14 +2,12 @@ import Session from 'supertokens-web-js/recipe/session';
 
 export default async function doesSessionExist() {
     if (await Session.doesSessionExist()) {
-        console.log("Session exists");
         // you can also check if the session is valid
         // await Session.attemptRefreshingSession();
         // user is logged in
         return true;
     } else {
         // user has not logged in yet
-        console.log("Session does not exist");
         localStorage.removeItem("userId");
         // you can redirect to the login page or show a message
         // window.location.href = "/login";
