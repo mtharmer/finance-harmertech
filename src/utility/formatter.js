57d9ef83f07a948b2ba6209b889d0a9cf7c283b6
Michael Harmer <187333727+mtharmer-wc@users.noreply.github.com>
2025-06-26 20:01:40 -0400

    HT-84 | Improved formatter catching

--- src/utility/formatter.js
+++ src/utility/formatter.js
@@ -1,15 +1,18 @@
 export function formatCurrency(amount) {
-  if (!Number(amount)) return amount;
+  if (!amount) return amount;
+  if (typeof amount === 'string') {
+    amount = Number(amount);
+  }
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  }).format(amount);
+  }).format(Number(amount))
 }
 
 export function formatPercent(amount) {
   if (!Number(amount)) return amount;
   return new Intl.NumberFormat('en-US', {
     style: 'percent',
-    maximumFractionDigits: 2
+    maximumFractionDigits: 3
   }).format(amount/100);
 }
